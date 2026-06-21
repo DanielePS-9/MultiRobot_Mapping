9633cb0fa3227b804ad80fa041d86af3d1fe524e
DanielePS-9 <danielesomma289@gmail.com>
2026-06-21 17:37:47 +0200

    slam

--- ros_ws/src/multirobot_mapping/launch/spawn_tb3.launch.py
+++ ros_ws/src/multirobot_mapping/launch/spawn_tb3.launch.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+ 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction, GroupAction, IncludeLaunchDescription
@@ -8,61 +8,71 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import PushRosNamespace
 from launch_ros.actions import Node
-
+ 
 # 1. Trova il percorso assoluto della cartella in cui si trova QUESTO file di launch
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
+ 
 # 2. Aggiungi questa cartella alla lista dei percorsi di Python
 sys.path.append(current_dir)
 from utils import load_sdf_with_namespace, create_namespaced_bridge_yaml
-
+ 
 def generate_launch_description():
     # Get the sdf file
     TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
-
+ 
     model_folder = 'turtlebot3_' + TURTLEBOT3_MODEL
-    
+   
+    pkg_slam_toolbox = get_package_share_directory('slam_toolbox')
+ 
     sdf_path = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
         'models',
         model_folder,
         'model.sdf'
     )
-
+ 
     bridge_path = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
         'params',
         model_folder+'_bridge.yaml'
     )
-
+ 
     # Launch configuration variables specific to simulation
     x_pose    = LaunchConfiguration('x_pose', default='0.0')
     y_pose    = LaunchConfiguration('y_pose', default='0.0')
     namespace = LaunchConfiguration('namespace', default='')
-
+ 
     # Declare the launch arguments
     declare_x_position_cmd = DeclareLaunchArgument(
         'x_pose', default_value='0.0',
         description='Initial X position')
-
+ 
     declare_y_position_cmd = DeclareLaunchArgument(
         'y_pose', default_value='0.0',
         description='Initial Y position')
-
+ 
     declare_namespace_cmd = DeclareLaunchArgument(
         'namespace', default_value='',
         description='Specify namespace of the robot')
-
-
-
+ 
+ 
+ 
     def spawn_rbt(context):
         namespace = context.launch_configurations['namespace']
         x_pose    = context.launch_configurations['x_pose']
         y_pose    = context.launch_configurations['y_pose']
-
+ 
+        use_sim_time_val = context.launch_configurations.get('use_sim_time', 'true')
+ 
         ns_sdf = load_sdf_with_namespace(sdf_path, namespace)
         ns_yaml = create_namespaced_bridge_yaml(bridge_path,namespace)
-
+ 
+        slam_params_file = os.path.join(
+            get_package_share_directory('multirobot_mapping'),
+            'params',
+            'slam_toolbox_' + f'{namespace}' + '.yaml'
+        )
+ 
         gazebo_ros_spawner = Node(
             package='ros_gz_sim',
             executable='create',
@@ -74,9 +84,9 @@ def generate_launch_description():
                 '-z', '0.01'
             ],
             output='screen',
-        ) 
-
-        robot_state_publisher = GroupAction([
+        )
+ 
+        robot_state_publisher_slam = GroupAction([
             PushRosNamespace(namespace),
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
@@ -86,9 +96,18 @@ def generate_launch_description():
                     'use_sim_time': 'true',
                     'frame_prefix': namespace
                 }.items()
-            )
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(pkg_slam_toolbox, 'launch', 'online_async_launch.py')
+                    ),
+                launch_arguments={
+                    'use_sim_time': use_sim_time_val,
+                    'slam_params_file': slam_params_file,
+                }.items(),
+            ),
         ])
-        
+       
         gazebo_ros_bridge = Node(
             package='ros_gz_bridge',
             executable='parameter_bridge',
@@ -99,17 +118,17 @@ def generate_launch_description():
             ],
             output='screen',
         )
-
-        return [gazebo_ros_spawner, gazebo_ros_bridge, robot_state_publisher]
-
+ 
+        return [gazebo_ros_spawner, gazebo_ros_bridge, robot_state_publisher_slam]
+ 
     ld = LaunchDescription()
-
+ 
     # Declare the launch options
     ld.add_action(declare_x_position_cmd)
     ld.add_action(declare_y_position_cmd)
     ld.add_action(declare_namespace_cmd)
-
+ 
     # Add any conditioned actions
     ld.add_action(OpaqueFunction(function = spawn_rbt))
-
+ 
     return ld
--- ros_ws/src/multirobot_mapping/launch/three_robots.launch.py
+++ ros_ws/src/multirobot_mapping/launch/three_robots.launch.py
@@ -1,52 +1,70 @@
 import os
-
+ 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import AppendEnvironmentVariable
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-
+ 
 from launch_ros.actions import Node
-
-
+ 
+ 
 def generate_launch_description():
     launch_file_dir = os.path.join(get_package_share_directory('multirobot_mapping'), 'launch')
     ros_gz_sim = get_package_share_directory('ros_gz_sim')
-
+ 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-
+ 
     x_pose_r1 = LaunchConfiguration('x_pose_r1', default='2.0')
     y_pose_r1 = LaunchConfiguration('y_pose_r1', default='0.0')
     ns_r1 = LaunchConfiguration('ns_r1', default='robot1')
-
+ 
     x_pose_r2 = LaunchConfiguration('x_pose_r2', default='1.0')
     y_pose_r2 = LaunchConfiguration('y_pose_r2', default='0.5')
     ns_r2 = LaunchConfiguration('ns_r2', default='robot2')
-
+ 
     x_pose_r3 = LaunchConfiguration('x_pose_r3', default='-2.0')
     y_pose_r3 = LaunchConfiguration('y_pose_r3', default='-0.5')
     ns_r3 = LaunchConfiguration('ns_r3', default='robot3')
-
+ 
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
         'worlds',
         'stanza.world'
     )
-
+ 
+    slam_r1 = os.path.join(
+        get_package_share_directory('multirobot_mapping'),
+        'params',
+        'slam_toolbox_robot1.yaml'
+    )
+ 
+    slam_r2 = os.path.join(
+        get_package_share_directory('multirobot_mapping'),
+        'params',
+        'slam_toolbox_robot2.yaml'
+    )
+   
+    slam_r3 = os.path.join(
+        get_package_share_directory('multirobot_mapping'),
+        'params',
+        'slam_toolbox_robot3.yaml'
+    )
+ 
     gazebo_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
         ),
         launch_arguments={'gz_args': ['-r ', world]}.items()
     )
-
+ 
     set_env_vars_resources = AppendEnvironmentVariable(
             'GZ_SIM_RESOURCE_PATH',
             os.path.join(
                 get_package_share_directory('turtlebot3_gazebo'),
                 'models'))
-    
+   
     clock_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
@@ -54,7 +72,7 @@ def generate_launch_description():
         arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
         output='screen'
     )
-
+ 
                                 # ROBOT1
     robot1_spawn_cmd = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
@@ -66,15 +84,16 @@ def generate_launch_description():
                 'namespace': ns_r1
             }.items()
         )
-    
+   
     robot1_tf_publisher = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
         name='world_to_robot1_odom',
         arguments=[x_pose_r1, y_pose_r1, '0.01', '0.0', '0.0', '0.0', 'world', 'robot1/odom']
     )
-    
-
+ 
+   
+ 
                                 # ROBOT2    
     robot2_spawn_cmd = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
@@ -86,14 +105,15 @@ def generate_launch_description():
                 'namespace': ns_r2
             }.items()
         )
-    
+   
     robot2_tf_publisher = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
         name='world_to_robot2_odom',
         arguments=[x_pose_r2, y_pose_r2, '0.01', '0.0', '0.0', '0.0', 'world', 'robot2/odom']
     )
-    
+ 
+   
                                 # ROBOT3    
     robot3_spawn_cmd = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
@@ -105,16 +125,17 @@ def generate_launch_description():
                 'namespace': ns_r3
             }.items()
         )
-    
+   
     robot3_tf_publisher = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
         name='world_to_robot3_odom',
         arguments=[x_pose_r3, y_pose_r3, '0.01', '0.0', '0.0', '0.0', 'world', 'robot3/odom']
     )
-
+ 
+ 
     ld = LaunchDescription()
-
+ 
     # Add the commands to the launch description
     ld.add_action(set_env_vars_resources)
     ld.add_action(gazebo_cmd)
