498492aa1ea1395495f822a98f998805935a13b6
DanielePS-9 <danielesomma289@gmail.com>
2026-07-10 10:40:53 +0200

    map launch mod

--- ros_ws/src/multirobot_mapping/launch/map.launch.py
+++ ros_ws/src/multirobot_mapping/launch/map.launch.py
@@ -4,10 +4,6 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, TimerAction
-from laimport os
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import ExecuteProcess, TimerAction
 from launch_ros.actions import Node
  
 def generate_launch_description():
